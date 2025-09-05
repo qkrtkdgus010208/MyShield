eb8390bf196dfc5c7fb851716dcf5ad438469fda
qkrtkdgus010208 <psh010208@naver.com>
2025-09-05 14:06:51 +0900

    기본 씬 구성

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = mousePos;
+    }
+}
