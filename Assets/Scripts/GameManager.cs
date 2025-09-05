f929c2150c50c52d32d301bd78aad00e5778c2a5
qkrtkdgus010208 <psh010208@naver.com>
2025-09-05 14:20:47 +0900

    장애물 만들기
    중력과 위치

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject square;
+
+    private void Start()
+    {
+        InvokeRepeating("MakeSquare", 0f, 1f);
+    }
+
+    private void MakeSquare()
+    {
+        Instantiate(square);
+    }
+}
