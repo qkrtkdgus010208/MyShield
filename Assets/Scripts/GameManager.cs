559b640410e342a33614d170716b8c5a679d3f51
qkrtkdgus010208 <psh010208@naver.com>
2025-09-05 14:24:46 +0900

    Feature/game flow (#2)
    * 장애물 만들기
    
    중력과 위치
    
    * 시간 구현하기

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject square;
+    [SerializeField] Text timeText;
+
+    private float time;
+
+    private void Start()
+    {
+        InvokeRepeating("MakeSquare", 0f, 1f);
+    }
+
+    private void Update()
+    {
+        time += Time.deltaTime;
+        timeText.text = time.ToString("F2");
+    }
+
+    private void MakeSquare()
+    {
+        Instantiate(square);
+    }
+}
---
+++ Assets/Scripts/Square.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Square : MonoBehaviour
+{
+    private void Start()
+    {
+        float x = Random.Range(-2.2f, 2.2f);
+        float y = Random.Range(3.0f, 5.0f);
+
+        transform.position = new Vector2(x, y);
+
+        float size = Random.Range(0.5f, 1.5f);
+        transform.localScale = new Vector2(size, size);
+    }
+}
