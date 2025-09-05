a06efde0f37fe6ca8c0ddd188fdac46a67c27530
qkrtkdgus010208 <psh010208@naver.com>
2025-09-05 15:15:42 +0900

    Feature/game over (#3)
    * 게임 종료 패널 제작
    
    * 게임 종료 로직 구현
    
    * 최고 점수 구현
    
    * 풍선 애니메이션 제작

---
+++ Assets/Scripts/RetryButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RetryButton : MonoBehaviour
+{
+    public void Retry()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
